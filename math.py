de0cbb7c426554d2136b5c65cbd1803d56676fbf
Nandani_Singh <singh.nandani.2011@gmail.com>
2022-03-09 23:41:27 +0530

    new insertion

--- math.py
+++ math.py
@@ -1,12 +1,17 @@
 #add implementation
+
 def add (x,y):
     pass
 #subtract implementation
+
 def sub (x,y):
     pass
 #multiply implementation
+
 def mul (x,y):
     pass
+#divison implementation
+
 def div (x,y):
     pass
 #this is a basic calculator type performing function app
