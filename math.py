1a2d3d5f829e68007f6f8f0971aba5fe887d0ae7
singhnandani <101272863+singhnandani@users.noreply.github.com>
2022-03-09 23:17:15 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+#add implementation
+def add (x,y):
+    pass
+#subtract implementation
+def sub (x,y):
+    pass
+#multiply implementation
+def mul (x,y):
+    pass
+def div (x,y):
+    pass
+#this is a basic calculator type performing function app
